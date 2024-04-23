98c162ce1770b6c4302194dcd215912469b4d4ad
tomblazejewski <tomasz.blazejewski.1b@gmail.com>
2024-04-23 22:26:06 +0100

    Add relative l numbers and nerd font, empty two files

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require 'keymaps'
+require 'options'
 --[[
 
 =====================================================================
@@ -91,7 +93,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +104,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
